9e03f74773a7b090168e4ba87a8715ed761fdb12
Kazuki Matsuda <kazuki.matsuda@intimatemerger.com>
2015-10-28 00:32:03 +0900

    add external python-librarys

---
+++ dmp/external/woothee-1.2.0.dist-info/DESCRIPTION.rst
@@ -0,0 +1,128 @@
+Woothee python
+==============
+
+|travis| |coveralls| |downloads| |version| |license|
+
+The Python implementation of Project Woothee, which is multi-language
+user-agent strings parsers.
+
+https://github.com/woothee/woothee
+
+Installation
+------------
+
+::
+
+   $ pip install woothee
+
+Usage
+-----
+
+Parsing user-agent
+~~~~~~~~~~~~~~~~~~
+
+.. code:: python
+
+    import woothee
+    woothee.parse("Mozilla/4.0 (compatible; MSIE 8.0; Windows NT 6.1; Trident/4.0)")
+    # => {'name': 'Internet Explorer', 'category': 'pc', 'os': 'Windows 7', 'version': '8.0', 'vendor': 'Microsoft', 'os_version': 'NT 6.1'}
+
+Parse user-agent string and returns a object with keys ``name``, ``category``, ``os``, ``version``, ``vendor`` and ``os_version``.
+
+For unknown user-agent (or partially failed to parse), result objects
+may have value 'UNKNOWN'.
+
+* ``category``
+
+  * labels of user terminal type, one of 'pc', 'smartphone', 'mobilephone', 'appliance', 'crawler' or 'misc' (or 'UNKNOWN')
+
+* ``name``
+
+  * the name of browser, like 'Internet Explorer', 'Firefox', 'GoogleBot'
+
+* ``version``
+
+  * version string, like '8.0' for IE, '9.0.1' for Firefix, '0.2.149.27' for Chrome, and so on
+
+* ``os``
+
+  * ex: 'Windows 7', 'Mac OSX', 'iPhone', 'iPad', 'Android'
+  * This field used to indicate cellar phone carrier for category 'mobilephone'
+
+* ``vendor``
+
+  * optional field, shows browser vendor
+
+* ``os_version``
+
+  * optional field, shows version of operating systems
+
+Finding crawlers (almost all, not all) in fast
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+.. code:: python
+
+   woothee.is_crawler('Mozilla/4.0 (compatible; MSIE 8.0; Windows NT 6.1; Trident/4.0)')
+   # => False
+
+Try to see useragent's category is 'crawler' or not, by casual(fast)
+method. Minor case of crawlers is not tested in this method. To check
+crawler strictly, use ``woothee.parse(str)['category'] == 'crawler'``.
+
+Authors
+-------
+
+* UEDA Tetsuhiro (najeira)
+* TAGOMORI Satoshi tagomoris@gmail.com
+* tell-k ffk2005@gmail.com
+
+License
+-------
+
+Copyright 2012- TAGOMORI Satoshi (tagomoris)
+
+Licensed under the Apache License, Version 2.0 (the "License"); you may
+not use this file except in compliance with the License. You may obtain
+a copy of the License at
+
+http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+
+History
+-------
+
+1.1.0(Mar 1, 2015)
+~~~~~~~~~~~~~~~~~~~~
+* `#9 Test blank cases <https://github.com/woothee/woothee-python/pull/9>`_ Thanks to yuya-takeyama.
+
+1.0.0(Jan 20, 2015)
+~~~~~~~~~~~~~~~~~~~~
+* First release
+
+
+.. |travis| image:: https://travis-ci.org/woothee/woothee-python.svg?branch=master
+    :target: https://travis-ci.org/woothee/woothee-python
+    :alt: travis-ci.org
+
+.. |coveralls| image:: https://coveralls.io/repos/woothee/woothee-python/badge.png
+    :target: https://coveralls.io/r/woothee/woothee-python
+    :alt: coveralls.io
+
+.. |downloads| image:: https://img.shields.io/pypi/dm/woothee.svg
+    :target: http://pypi.python.org/pypi/woothee/
+    :alt: downloads
+
+.. |version| image:: https://img.shields.io/pypi/v/woothee.svg
+    :target: http://pypi.python.org/pypi/woothee/
+    :alt: latest version
+
+.. |license| image:: https://img.shields.io/pypi/l/woothee.svg
+    :target: http://pypi.python.org/pypi/woothee/
+    :alt: license
+
+
---
+++ dmp/external/woothee/browser.py
@@ -0,0 +1,123 @@
+# -*- coding: utf-8 -*-
+from __future__ import (division, print_function,
+                        absolute_import, unicode_literals)
+
+import re
+from . import dataset
+from . import util
+
+
+def challenge_msie(ua, result):
+    if 'compatible; MSIE' not in ua and 'Trident/' not in ua\
+            and 'IEMobile' not in ua:
+        return False
+    version = dataset.VALUE_UNKNOWN
+    msie = re.search(r'MSIE ([.0-9]+);', ua)
+    trident = re.search(
+        r'Trident\/([.0-9]+);', ua)
+    tridentVersion = re.search(r' rv:([.0-9]+)', ua)
+    iemobile = re.search(r'IEMobile\/([.0-9]+);', ua)
+
+    if msie:
+        version = msie.group(1)
+    elif trident and tridentVersion:
+        version = tridentVersion.group(1)
+    elif iemobile:
+        version = iemobile.group(1)
+
+    util.update_map(result, dataset.get('MSIE'))
+    util.update_version(result, version)
+    return True
+
+
+def challenge_safari_chrome(ua, result):
+    if 'Safari/' not in ua:
+        return False
+    version = dataset.VALUE_UNKNOWN
+
+    # Edge
+    obj = re.search('Edge\/([.0-9]+)', ua)
+    if obj:
+        version = obj.group(1)
+        util.update_map(result, dataset.get('Edge'))
+        util.update_version(result, version)
+        return True
+
+    obj = re.search('(?:Chrome|CrMo|CriOS)/([.0-9]+)', ua)
+    if obj:
+        chromeVersion = obj.group(1)
+        obj = re.search('OPR/([.0-9]+)', ua)
+        if obj:
+            # Opera (blink)
+            version = obj.group(1)
+            util.update_map(result, dataset.get('Opera'))
+            util.update_version(result, version)
+            return True
+
+        # Chrome
+        util.update_map(result, dataset.get('Chrome'))
+        util.update_version(result, chromeVersion)
+        return True
+
+    # Safari
+    obj = re.search('Version/([.0-9]+)', ua)
+    if obj:
+        version = obj.group(1)
+    util.update_map(result, dataset.get('Safari'))
+    util.update_version(result, version)
+    return True
+
+
+def challenge_firefox(ua, result):
+    if 'Firefox/' not in ua:
+        return False
+    obj = re.search('Firefox/([.0-9]+)', ua)
+    version = obj.group(1) if obj else dataset.VALUE_UNKNOWN
+    util.update_map(result, dataset.get('Firefox'))
+    util.update_version(result, version)
+    return True
+
+
+def challenge_opera(ua, result):
+    if 'Opera' not in ua:
+        return False
+    obj = re.search('Version/([.0-9]+)', ua)
+    version = dataset.VALUE_UNKNOWN
+    if obj:
+        version = obj.group(1)
+    else:
+        obj = re.search('Opera[/ ]([.0-9]+)', ua)
+        if obj:
+            version = obj.group(1)
+    util.update_map(result, dataset.get('Opera'))
+    util.update_version(result, version)
+    return True
+
+
+def challenge_webview(ua, result):
+    obj = re.search('iP(?:hone;|ad;|od) .*like Mac OS X', ua)
+    # iOS
+    if not obj or 'Safari/' in ua:
+        return False
+
+    obj = re.search('Version\/([.0-9]+)', ua)
+    version = obj.group(1) if obj else dataset.VALUE_UNKNOWN
+    util.update_map(result, dataset.get('Webview'))
+    util.update_version(result, version)
+    return True
+
+
+def challenge_sleipnir(ua, result):
+    if 'Sleipnir/' not in ua:
+        return False
+    obj = re.search('Sleipnir/([.0-9]+)', ua)
+    version = obj.group(1) if obj else dataset.VALUE_UNKNOWN
+    util.update_map(result, dataset.get('Sleipnir'))
+    util.update_version(result, version)
+    # Sleipnir's user-agent doesn't contain Windows version,
+    # so put 'Windows UNKNOWN Ver'.
+    # Sleipnir is IE component browser, so for Windows only.
+    win = dataset.get('Win')
+    util.update_category(result, win[dataset.KEY_CATEGORY])
+    util.update_os(result, win[dataset.KEY_NAME])
+    return True
